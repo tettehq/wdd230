157e2ce03d271dbbcdbee8f1ccf7372ac3ca56bf
tettehq <ronyquash@gmail.com>
2021-09-18 18:07:54 +0000

    update

--- js/myscript.js
+++ js/myscript.js
@@ -1,38 +1,37 @@
 
-let dayNames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-]
+// let dayNames = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday"
+// ]
 
-let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-]
+// let months = [
+//     "January",
+//     "February",
+//     "March",
+//     "April",
+//     "May",
+//     "June",
+//     "July",
+//     "August",
+//     "September",
+//     "October",
+//     "November",
+//     "December"
+// ]
 
 let d = new Date()
+document.getElementById("currentyear").innerHTML = d.getFullYear()
 
-let dayName = dayNames[d.getDay()]
+/*let dayName = dayNames[d.getDay()]
 let month = months[d.getMonth()]
 let year = d.getFullYear()
 let time = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`
 
-let fulldatetime = `${dayName}, ${month} ${d.getDate()}, ${year} ${time}`
+let fulldatetime = `${dayName}, ${month} ${d.getDate()}, ${year} ${time}`*/
 
-document.getElementById("currentdatetime").innerHTML = `Last updated: ${fulldatetime}`
-
-document.getElementById("info").innerHTML = `&copy; ${year} | Aaron Quarshie | Ghana | <a href="https://www.byui.edu/online" target="blank">BYUI Online Learning</a>`
+document.getElementById("currentdatetime").innerHTML = `Last updated: ${new Date(document.lastModified)}`
